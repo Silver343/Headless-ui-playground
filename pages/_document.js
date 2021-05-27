c06aeb44090b9627d0725389eb28e8858e7a3fd7
Silver343 <51054165+Silver343@users.noreply.github.com>
2021-05-27 16:12:21 +0000

    moved defIault tite

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,10 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <title>Headless UI Playgound</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
         <body className="bg-gray-800">
           <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
-import Head from 'next/head'
 import Nav from '../components/Nav'
 
 export default function Home() {
   return (
     <div >
-      <Head>
-        <title>Headless UI Playgound</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main>
         <h1 className="my-5 text-gray-100 text-2xl font-semibold md:text-3xl text-center">
           This is a play ground site for <a href="https://headlessui.dev/" >Headless UI </a>
